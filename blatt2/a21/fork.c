e0aa2b9d4f279b0757336fa3fa2b773d89a90e70
stnwtr <simon.steinkellner.ss@gmail.com>
2024-05-02 09:37:13 +0200

    Add fork program a21

---
+++ blatt2/a21/fork.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        printf("Fehler beim erzeugen eines neuen Prozesses!\n");
+        return 1;
+    }
+
+    if (pid == 0) {
+        printf("Kind! pid = %d, ppid = %d\n", getpid(), getppid());
+    } else {
+        printf("Vater! pid = %d, ppid = %d\n", getpid(), getppid());
+    }
+
+    while (true);
+
+    return 0;
+}
